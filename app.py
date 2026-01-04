c4c008329a5845fc28f245b000ebe292aea51e26
unknown <Paris Ariyan>
2026-01-04 13:03:48 +0600

    added

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ apk_controller = APKController(processor)
 
 @app.route("/", methods=["GET"])
 def home():
-    return "404 not found - 1.1.1"
+    return "404 not found - 1.1.2"
 
 @app.route("/harden", methods=["POST"])
 def harden():
