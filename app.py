c4efd015b10ba8e9aa0dbf918935e832f1770fe7
unknown <Paris Ariyan>
2026-02-06 22:58:46 +0600

    admin dashboard added

--- app.py
+++ app.py
@@ -66,5 +66,10 @@ def jobStatus():
     return jsonify({"status": "ok"}), 200
 
 
+@socketio.on('notify')
+def handle_notify(data):
+    socketio.emit('notify', data)
+
+
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0", port=8000, debug=True)
